578b1e57f1e6cd8cb846a9680019e1edca474c59
Frank Wang <deas.wang@gmail.com>
2018-09-06 16:39:50 +0800

    add

---
+++ golang/111.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    if root.Left == nil && root.Right == nil {
+        return 1
+    }
+    if root.Left == nil {
+        return minDepth(root.Right)+1
+    }
+    if root.Right == nil {
+        return minDepth(root.Left)+1
+    }
+    left := minDepth(root.Left)
+    right := minDepth(root.Right)
+    if left>right {
+        return right +1
+    } else {
+        return left+1
+    }
+}
+ 
